9bf43329c500bde0038a02f569d7f1601f4696b5
tiffrachel <116373674+tiffrachel@users.noreply.github.com>
2023-03-31 09:18:01 +0700

    comm

---
+++ service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'portfolio-cache-v1';
+const urlsToCache = [
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/about.html',
+  '/blog.html',
+  '/contact.html',
+  '/portfolio-example01.html',
+];
+
+// Menginstall service worker dan melakukan caching pada file-file yang diperlukan
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+  
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        // Mengembalikan data dari cache jika ada
+        if (response) {
+          return response;
+        }
+  
+        // Jika tidak ada di cache, maka ambil dari network
+        return fetch(event.request).then((response) => {
+          // Simpan data response dari network ke dalam cache
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
+  
+  // Membersihkan cache yang sudah tidak digunakan lagi
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.filter(function(cacheName) {
+            return cacheName !== CACHE_NAME;
+          }).map(function(cacheName) {
+            return caches.delete(cacheName);
+          })
+        );
+      })
+    );
+  });
+
+
