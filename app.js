a8db6e3dba2f6e46f6f5f6a873c6ffa838ebb94e
tiffrachel <116373674+tiffrachel@users.noreply.github.com>
2023-03-31 09:48:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-let deferredPrompt;
+navigator.serviceWorker.register('service-worker.js');
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  showInstallButton();
+var deferredPrompt;
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
-
-function showInstallButton() {
-  const installButton = document.getElementById('install-button');
-  installButton.style.display = 'block';
-
-  installButton.addEventListener('click', (e) => {
-    installButton.style.display = 'none';
-    deferredPrompt.prompt();
-
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('Pengguna telah menginstal aplikasi');
-      } else {
-        console.log('Pengguna tidak menginstal aplikasi');
-      }
-      deferredPrompt = null;
-    });
-  });
-}
